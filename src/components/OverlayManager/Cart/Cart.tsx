259024b6d9aa2fbce3f1f85af1631b0c3a2e39ee
Francisco Rodriguez <72564438+franncode-tb@users.noreply.github.com>
2020-11-13 12:14:57 -0300

    [FR-351] Delete item confirm modal (#68)
    * feat: delete item confirm modal
    
    * fix: DeleteModal styles
    
    * fix: DeleteModal styles and wordings
    
    Co-authored-by: Francisco Rodriguez <fjrodriguez.353@gmail.com>

--- src/components/OverlayManager/Cart/Cart.tsx
+++ src/components/OverlayManager/Cart/Cart.tsx
@@ -1,12 +1,9 @@
-import "./scss/index.scss";
-
 import * as React from "react";
 import { Link } from "react-router-dom";
 import ReactSVG from "react-svg";
-
 import { TaxedMoney } from "@components/containers";
 import { useCart, useCheckout, useUserDetails } from "@sdk/react";
-
+import "./scss/index.scss";
 import {
   Button,
   Offline,
@@ -16,14 +13,16 @@ import {
   OverlayContextInterface,
 } from "../..";
 import { checkoutLoginUrl, checkoutUrl } from "../../../app/routes";
+import DeleteModal from "./DeleteModal";
 import Empty from "./Empty";
 import ProductList from "./ProductList";
-
 import aunaImg from "../../../images/logo.svg";
 import closeImg from "../../../images/close-circle.svg";
 import cartImg from "../../../images/cart-light.svg";
 
 const Cart: React.FC<{ overlay: OverlayContextInterface }> = ({ overlay }) => {
+  const [isModelOpen, setIsModelOpen] = React.useState(false);
+  const [itemToDelete, setItemToDelete] = React.useState(null);
   const { data: user } = useUserDetails();
   const { checkout } = useCheckout();
   const {
@@ -53,97 +52,120 @@ const Cart: React.FC<{ overlay: OverlayContextInterface }> = ({ overlay }) => {
     0
   );
 
+  const showModal = (itemId: string) => {
+    setItemToDelete(itemId);
+    setIsModelOpen(true);
+  };
+
+  const removeItemAndHideModal = () => {
+    removeItem(itemToDelete);
+    setIsModelOpen(false);
+  };
+
   return (
     <Overlay context={overlay}>
       <Online>
-        <div className="cart">
-          <div className="overlay__header">
-            <div className="overlay__header__top">
-              <div />
-              <ReactSVG
-                path={aunaImg}
-                onClick={overlay.hide}
-                className="overlay__header__close-icon"
-              />
-              <ReactSVG
-                path={closeImg}
-                onClick={overlay.hide}
-                className="overlay__header__close-icon"
-              />
-            </div>
-            <div className="overlay__header__bottom">
-              <div className="overlay__header-text">
-                Tu carrito{" "}
-                <span className="overlay__header-text-items">
-                  {totalProducts || 0}{" "}
-                  {totalProducts === 1 ? "producto" : "productos"}
-                </span>
-              </div>
-              <ReactSVG path={cartImg} className="overlay__header__cart-icon" />
-            </div>
+        {isModelOpen ? (
+          <div className="cart">
+            <DeleteModal
+              onConfirm={() => removeItemAndHideModal()}
+              onCancel={() => setIsModelOpen(false)}
+              onClose={() => setIsModelOpen(false)}
+            />
           </div>
-          {items?.length ? (
-            <>
-              <ProductList
-                lines={items}
-                onRemove={removeItem}
-                onQuantity={addItem}
-              />
-              <div className="cart__footer">
-                <div className="cart__footer__price cart__footer__price--sub">
-                  <span>Subtotal</span>
-                  <span>
-                    <TaxedMoney
-                      data-cy="cartPageSubtotalPrice"
-                      taxedMoney={subtotalPrice}
-                    />
-                  </span>
-                </div>
-
-                {shippingTaxedPrice && shippingTaxedPrice.gross.amount !== 0 && (
-                  <div className="cart__footer__price">
-                    <span>Shipping</span>
-                    <span>
-                      <TaxedMoney
-                        data-cy="cartPageShippingPrice"
-                        taxedMoney={shippingTaxedPrice}
-                      />
-                    </span>
-                  </div>
-                )}
-
-                {promoTaxedPrice && promoTaxedPrice.gross.amount !== 0 && (
-                  <div className="cart__footer__price">
-                    <span>Promo code</span>
-                    <span>
-                      <TaxedMoney
-                        data-cy="cartPagePromoCodePrice"
-                        taxedMoney={promoTaxedPrice}
-                      />
-                    </span>
-                  </div>
-                )}
-
-                <div className="cart__footer__price">
-                  <span>Total</span>
-                  <span>
-                    <TaxedMoney
-                      data-cy="cartPageTotalPrice"
-                      taxedMoney={totalPrice}
-                    />
-                  </span>
-                </div>
-                <div className="cart__footer__button">
-                  <Link to={user ? checkoutUrl : checkoutLoginUrl}>
-                    <Button>Comprar</Button>
-                  </Link>
-                </div>
+        ) : (
+          <div className="cart">
+            <div className="overlay__header">
+              <div className="overlay__header__top">
+                <div />
+                <ReactSVG
+                  path={aunaImg}
+                  onClick={overlay.hide}
+                  className="overlay__header__close-icon"
+                />
+                <ReactSVG
+                  path={closeImg}
+                  onClick={overlay.hide}
+                  className="overlay__header__close-icon"
+                />
               </div>
-            </>
-          ) : (
-            <Empty overlayHide={overlay.showCatalog} />
-          )}
-        </div>
+              <div className="overlay__header__bottom">
+                <div className="overlay__header-text">
+                  Tu carrito{" "}
+                  <span className="overlay__header-text-items">
+                    {totalProducts || 0}{" "}
+                    {totalProducts === 1 ? "producto" : "productos"}
+                  </span>
+                </div>
+                <ReactSVG
+                  path={cartImg}
+                  className="overlay__header__cart-icon"
+                />
+              </div>
+            </div>
+            {items?.length ? (
+              <>
+                <ProductList
+                  lines={items}
+                  onRemove={showModal}
+                  onQuantity={addItem}
+                />
+                <div className="cart__footer">
+                  <div className="cart__footer__price cart__footer__price--sub">
+                    <span>Subtotal</span>
+                    <span>
+                      <TaxedMoney
+                        data-cy="cartPageSubtotalPrice"
+                        taxedMoney={subtotalPrice}
+                      />
+                    </span>
+                  </div>
+
+                  {shippingTaxedPrice && shippingTaxedPrice.gross.amount !== 0 && (
+                    <div className="cart__footer__price">
+                      <span>Shipping</span>
+                      <span>
+                        <TaxedMoney
+                          data-cy="cartPageShippingPrice"
+                          taxedMoney={shippingTaxedPrice}
+                        />
+                      </span>
+                    </div>
+                  )}
+
+                  {promoTaxedPrice && promoTaxedPrice.gross.amount !== 0 && (
+                    <div className="cart__footer__price">
+                      <span>Promo code</span>
+                      <span>
+                        <TaxedMoney
+                          data-cy="cartPagePromoCodePrice"
+                          taxedMoney={promoTaxedPrice}
+                        />
+                      </span>
+                    </div>
+                  )}
+
+                  <div className="cart__footer__price">
+                    <span>Total</span>
+                    <span>
+                      <TaxedMoney
+                        data-cy="cartPageTotalPrice"
+                        taxedMoney={totalPrice}
+                      />
+                    </span>
+                  </div>
+                  <div className="cart__footer__button">
+                    <Link to={user ? checkoutUrl : checkoutLoginUrl}>
+                      <Button>Comprar</Button>
+                    </Link>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <Empty overlayHide={overlay.showCatalog} />
+            )}
+          </div>
+        )}
       </Online>
       {/* // TODO: revisar comportamiento offline */}
       <Offline>
