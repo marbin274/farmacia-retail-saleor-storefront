5c1c20e2fb8a249036831428f8f60cfb739c71ae
Juan Condori Olivo <30666026+juancondori@users.noreply.github.com>
2020-12-23 13:37:31 -0500

    FR-573 fix: Registration button after order placement does nothing (#133)
    * fix:view
    
    * fix:view
    
    * Update ThankYouPage.tsx
    
    Co-authored-by: Jersson Dongo <jersson@gmail.com>

--- src/@next/pages/ThankYouPage/ThankYouPage.tsx
+++ src/@next/pages/ThankYouPage/ThankYouPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
 import { ThankYou } from "@components/organisms";
@@ -6,16 +6,19 @@ import { BASE_URL } from "@temp/core/config";
 import { generateGuestOrderDetailsUrl } from "@utils/core";
 
 import { IProps } from "./types";
+import { OverlayContext } from "@temp/components/Overlay"
 
 const ThankYouPage: React.FC<IProps> = ({}: IProps) => {
   const location = useLocation();
   const history = useHistory();
   const { token, orderNumber } = location.state;
+  const overlay = useContext(OverlayContext);
   return (
     <ThankYou
       continueShopping={() => history.push(BASE_URL)}
       orderNumber={orderNumber}
       orderDetails={() => history.push(generateGuestOrderDetailsUrl(token))}
+      overlay={overlay}
     />
   );
 };
