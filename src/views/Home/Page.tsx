d1f9317af3ce8c5146b44c00b068b47a451682b9
Josue Retamozo Vargas <josretamozovar@gmail.com>
2021-01-27 12:51:51 -0500

    chore(home-view): update banner 27/01 (#249)
    Co-authored-by: Josue Retamozo Vargas <jretamozov@auna.pe>

--- src/views/Home/Page.tsx
+++ src/views/Home/Page.tsx
@@ -21,14 +21,14 @@ const Page: React.FC<IPageProps> = ({
 }) => {
   return (
     <div className="container">
-      <a href="./product/crema-corporal-isdin-avena-400ml/2373/">
+      <a href="./product/pack-juntos-contra-el-covid/4700/">
         <div className="home-page__top-banner">
           <div className="home-page__top-banner__text">
-            <h2>Crema Corporal ISDIN</h2>
-            <h3><strong>Avena 400ml</strong></h3>
+            <h2>¡SOLO POR HOY!</h2>
+            <h3><strong>PACK PROTECCIÓN COVID</strong></h3>
             <p>
-              *Válido hasta el 31/01/21 o hasta agotar stock. Máx. 3 unids por producto.<br />
-              Aplica máx. 2 promociones por usuario. Delivery a S/0.01 hasta el 31/01/21. T&C.: <a href="http://bit.ly/TyC_Farmauna" target="_blank">http://bit.ly/TyC_Farmauna</a>
+              * Válido el 27/01/21 o hasta agotar stock. Max 3 unids por producto.
+              <br/> Aplica max. 2 promociones por usuario. Delivery S/0.01 hasta el 31/01/21. T&C:  <a href="http://bit.ly/TyC_Farmauna" target="_blank">http://bit.ly/TyC_Farmauna</a>
             </p>
           </div>
         </div>
