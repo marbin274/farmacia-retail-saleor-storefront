fafbaa7b1ea5fbc1fe4208be8a4289fea11c135e
Jose Carbonell <73846242+Jose-Carbonell@users.noreply.github.com>
2021-02-01 17:12:46 -0300

    feat: adding overflow to modal in register address (#265)
    * feat: adding overflow to modal in register address
    
    * fix: change hard code color by a constant
    
    Co-authored-by: Jersson Dongo <jersson@gmail.com>

--- src/@next/components/organisms/Modal/styles.ts
+++ src/@next/components/organisms/Modal/styles.ts
@@ -1,10 +1,11 @@
 import { styled } from "@styles";
 
-export const Modal = styled.div`  
+export const Modal = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   width: 100%;
+  overflow-y: auto;
   background-color: ${props => props.theme.colors.white};
 `;
 
