877c847edc44e66067894e4841ca407166bb32ac
Jose Carbonell <73846242+Jose-Carbonell@users.noreply.github.com>
2021-03-05 16:25:20 -0300

    feat: adding performance features (#409) #patch

--- config/webpack/config.prod.js
+++ config/webpack/config.prod.js
@@ -1,5 +1,5 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const BrotliGzipPlugin = require("brotli-gzip-webpack-plugin");
 module.exports = ({ sourceDir, distDir }) => ({
   output: {
     filename: "js/[name].[contenthash].js",
@@ -23,6 +23,20 @@ module.exports = ({ sourceDir, distDir }) => ({
     new MiniCssExtractPlugin({
       filename: "[name].[hash].css",
       chunkFilename: "[id].[hash].css",
+    }),
+    new BrotliGzipPlugin({
+      asset: "[path].br[query]",
+      algorithm: "brotli",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new BrotliGzipPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
     }),
   ],
 });
