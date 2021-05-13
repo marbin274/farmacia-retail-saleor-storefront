cd0a3c5c9327d4f2f1683e9d5c32a05ec4cbd191
Diego Machaca <diego.machaca@gmail.com>
2021-05-13 14:54:35 -0500

    Revert "chore: add link canonical (#635) #patch" (#643) #revert
    This reverts commit b18b071af3773c5df6f38140b82ae6ab4099e916.

--- src/components/Meta/consumer.tsx
+++ src/components/Meta/consumer.tsx
@@ -3,34 +3,26 @@ import { Helmet } from "react-helmet";
 
 import { Consumer as MetaConsumer } from "./context";
 
-const Consumer: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-  return (
-    <MetaConsumer>
-      {({ title, description, image, type, url, custom }) => (
-        <>
-          <Helmet
-            title={title}
-            link={[
-              {
-                href: url.split(/[?#]/)[0],
-                rel: "canonical",
-              },
-            ]}
-            meta={[
-              { name: "description", content: description },
-              { property: "og:url", content: url },
-              { property: "og:title", content: title },
-              { property: "og:description", content: description },
-              { property: "og:type", content: type },
-              { property: "og:image", content: image },
-              ...custom,
-            ]}
-          />
-          {children}
-        </>
-      )}
-    </MetaConsumer>
-  );
-};
+const Consumer: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
+  <MetaConsumer>
+    {({ title, description, image, type, url, custom }) => (
+      <>
+        <Helmet
+          title={title}
+          meta={[
+            { name: "description", content: description },
+            { property: "og:url", content: url },
+            { property: "og:title", content: title },
+            { property: "og:description", content: description },
+            { property: "og:type", content: type },
+            { property: "og:image", content: image },
+            ...custom,
+          ]}
+        />
+        {children}
+      </>
+    )}
+  </MetaConsumer>
+);
 
 export default Consumer;
