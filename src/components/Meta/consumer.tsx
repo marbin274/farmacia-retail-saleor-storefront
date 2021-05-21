94cd909ae971b0a154e5f11972e43ff544413656
Josue Retamozo Vargas <josretamozovar@gmail.com>
2021-05-21 11:32:08 -0500

    fix: case when canonical url where not the main url (#673) #patch

--- src/components/Meta/consumer.tsx
+++ src/components/Meta/consumer.tsx
@@ -6,29 +6,43 @@ import { Consumer as MetaConsumer } from "./context";
 const Consumer: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
   <MetaConsumer>
     {({ title, description, image, type, url, custom }) => {
-      const pathname: string = document.location.pathname?.includes("search") ? "/" : document.location.pathname;
-      const canonicalURL: string = `${document.location.protocol}//${document.location.hostname}${pathname}`;      
-      return <>
-        <Helmet
-          title={title}
-          link={[
-            {
-              href: canonicalURL,
-              rel: "canonical",
-            },
-          ]}
-          meta={[
-            { name: "description", content: description },
-            { property: "og:url", content: url },
-            { property: "og:title", content: title },
-            { property: "og:description", content: description },
-            { property: "og:type", content: type },
-            { property: "og:image", content: image },
-            ...custom,
-          ]}
-        />
-        {children}
-      </>
+      const pathname: string = document.location.pathname?.includes("search")
+        ? "/"
+        : document.location.pathname;
+
+      let canonicalURL: string = `${document.location.protocol}//${document.location.hostname}${pathname}`;
+      const homeCanonicalURL: string = canonicalURL.lastIndexOf("/")
+        ? canonicalURL.slice(0, -1)
+        : canonicalURL;
+      const homeCanonicalUrlSemiPath = `${document.location.protocol}//${document.location.hostname}`;
+
+      if (homeCanonicalUrlSemiPath === homeCanonicalURL) {
+        canonicalURL = homeCanonicalURL;
+      }
+
+      return (
+        <>
+          <Helmet
+            title={title}
+            link={[
+              {
+                href: canonicalURL,
+                rel: "canonical",
+              },
+            ]}
+            meta={[
+              { name: "description", content: description },
+              { property: "og:url", content: url },
+              { property: "og:title", content: title },
+              { property: "og:description", content: description },
+              { property: "og:type", content: type },
+              { property: "og:image", content: image },
+              ...custom,
+            ]}
+          />
+          {children}
+        </>
+      );
     }}
   </MetaConsumer>
 );
