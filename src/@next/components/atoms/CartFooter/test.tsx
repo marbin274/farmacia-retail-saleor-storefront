cc138d8d5c85bf721ccb06f1b54d240ddaafab78
Carlos <carlos.hs.92@gmail.com>
2021-07-02 11:51:43 -0500

    tests: update CartFooter tests and remove unuseful components (#914) #patch
    Co-authored-by: Diego Machaca <diego.machaca@gmail.com>

--- src/@next/components/atoms/CartFooter/test.tsx
+++ src/@next/components/atoms/CartFooter/test.tsx
@@ -1,4 +1,5 @@
-import { shallow } from "enzyme";
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import "jest-styled-components";
 import React from "react";
 
@@ -6,9 +7,36 @@ import { CartFooter } from ".";
 import { DEFAULT_PROPS } from "./fixtures";
 
 describe("<CartFooter />", () => {
-  it("exists", () => {
-    const wrapper = shallow(<CartFooter {...DEFAULT_PROPS} />);
+  let props: any;
 
-    expect(wrapper.exists()).toEqual(true);
+  beforeEach(() => {
+    props = { ...DEFAULT_PROPS };
   });
+
+  it("render with basic information", () => {
+    render(<CartFooter {...props} />);
+    expect(screen.queryByRole('totalPrice').textContent).toBe(props.totalPrice);
+    expect(screen.queryByRole('subtotalPrice').textContent).toBe(props.subtotalPrice);
+    expect(screen.queryByRole('shippingPrice')).not.toBeInTheDocument();
+    expect(screen.queryByRole('discountPrice')).not.toBeInTheDocument();
+  });
+
+  it("render with shippingPrice", () => {
+    props.shippingPrice = 'S/ 10.00';
+    render(<CartFooter {...props} />);
+    expect(screen.queryByRole('totalPrice').textContent).toBe(props.totalPrice);
+    expect(screen.queryByRole('subtotalPrice').textContent).toBe(props.subtotalPrice);
+    expect(screen.queryByRole('shippingPrice').textContent).toBe('S/ 10.00');
+    expect(screen.queryByRole('discountPrice')).not.toBeInTheDocument();
+  });
+
+  it("render with discountPrice", () => {
+    props.discountPrice = 'S/ 18.60';
+    render(<CartFooter {...props} />);
+    expect(screen.queryByRole('totalPrice').textContent).toBe(props.totalPrice);
+    expect(screen.queryByRole('subtotalPrice').textContent).toBe(props.subtotalPrice);
+    expect(screen.queryByRole('shippingPrice')).not.toBeInTheDocument();
+    expect(screen.queryByRole('discountPrice').textContent).toBe('S/ 18.60');
+  });
+
 });
