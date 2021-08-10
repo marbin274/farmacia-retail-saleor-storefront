07c4006fde01f9011b5725221dad764a57bf4108
Marbin Rusbelt Ramírez Chávez <marbin.027@gmail.com>
2021-08-10 14:04:58 -0500

    chore: fix new line

--- src/@sdk/consts.ts
+++ src/@sdk/consts.ts
@@ -3,4 +3,3 @@ export const WINDOW_EXISTS = typeof window !== "undefined";
 export const LOCAL_STORAGE_EXISTS = WINDOW_EXISTS && !!window.localStorage;
 export const CREDENTIAL_API_EXISTS =
   WINDOW_EXISTS && !!window.PasswordCredential;
-  
