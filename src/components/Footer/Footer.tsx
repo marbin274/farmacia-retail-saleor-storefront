e3f950369043808adfb271fc5a2c19c6737af93c
Gian Gabriel Caycho <gian22_02@hotmail.com>
2021-08-11 15:26:35 -0500

    feat(footer): change styles to tailwind/styled and remove unncesary logic

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,19 @@
-import "./scss/index.scss";
-import * as React from "react";
-import Nav from "./Nav";
-import { useLocation } from "react-router-dom";
+import './scss/index.scss';
+import * as React from 'react';
+import Nav from './Nav';
+import { useLocation } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   const location = useLocation();
   const showFooter = !(
-    location.pathname.includes("checkout") ||
-    location.pathname.includes("order-finalized")
+    location.pathname.includes('checkout') ||
+    location.pathname.includes('order-finalized')
   );
 
   if (!showFooter) return <></>;
 
   return (
-    <div className="footer" id="footer">
+    <div id="footer">
       <Nav />
     </div>
   );
