fa701a9509eca48820fcf63f71c902d2fa0e25a4
Marbin Rusbelt Ramírez Chávez <marbin.027@gmail.com>
2021-09-01 11:45:36 -0500

    chore: update config with prettier

--- src/@sdk/apollo.config.js
+++ src/@sdk/apollo.config.js
@@ -2,8 +2,8 @@ module.exports = {
   client: {
     includes: ['./queries/*.ts', './mutations/*.ts', './fragments/*.ts'],
     service: {
-      name: "saleor",
-      url: "http://localhost:8000/graphql/"
-    }
-  }
+      name: 'saleor',
+      url: 'http://localhost:8000/graphql/',
+    },
+  },
 };
