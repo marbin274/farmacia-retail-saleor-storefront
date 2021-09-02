a4617fd6bf1652c7c60a4231766efac8d22789bc
Diego Machaca <diego.machaca@gmail.com>
2021-09-02 13:03:28 -0500

    chore: clean apollo.config in root directory

--- apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    excludes: ['**/__tests__/**/*', '**/@sdk/**/*'],
-    service: {
-      name: 'saleor',
-      url: 'http://localhost:8000/graphql/',
-    },
-  },
-};
