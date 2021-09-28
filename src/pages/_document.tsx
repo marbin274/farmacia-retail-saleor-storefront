b0ea673bffddd53ebeb8a4a97ed46ba2e4676c59
Josue Retamozo Vargas <josretamozovar@gmail.com>
2021-09-28 17:19:09 -0500

    chore: use Script tag (#1229) #patch

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -59,24 +59,7 @@ export default class MyDocument extends Document {
             }}
           />
 
-          {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GA_TRACKING_ID}');`,
-            }}
-          />
-          {/* End Google Tag Manager */}
-          {/* Google Tag Manager (noscript) */}
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
-          {/* End Google Tag Manager (noscript) */}
+
 
         </Head>
         <body>
@@ -99,6 +82,14 @@ export default class MyDocument extends Document {
           `,
             }}
           ></script>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
         </body>
       </Html>
     );
