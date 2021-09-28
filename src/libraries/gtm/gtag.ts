a53eeaa0c0e82a914a6cc0d25393d784ffd90244
Diego Machaca <diego.machaca@gmail.com>
2021-09-28 14:35:43 -0500

    fix: delete useEffect (GTMPageView) (#1227) #patch

--- src/libraries/gtm/gtag.ts
+++ src/libraries/gtm/gtag.ts
@@ -6,19 +6,3 @@ declare global {
     gtag:any;
   }
 }
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
-  window?.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  })
-}
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value,
-  })
-}
