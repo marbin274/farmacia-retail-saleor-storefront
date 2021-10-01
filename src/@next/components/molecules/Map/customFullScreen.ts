bdc5486a36261540b032cfc45356b6562ebfd23b
marbin274 <marbin.027@gmail.com>
2021-09-30 19:21:01 -0500

    fix(3012): add full screen to many devices (#1242) #patch

--- src/@next/components/molecules/Map/customFullScreen.ts
+++ src/@next/components/molecules/Map/customFullScreen.ts
@@ -1,3 +1,23 @@
+declare global {
+  interface Document {
+    mozCancelFullScreen?: () => Promise<void>;
+    msExitFullscreen?: () => Promise<void>;
+    webkitExitFullscreen?: () => Promise<void>;
+    mozFullScreenElement?: Element;
+    msFullscreenElement?: Element;
+    webkitFullscreenElement?: Element;
+    onwebkitfullscreenchange?: any;
+    onmsfullscreenchange?: any;
+    onmozfullscreenchange?: any;
+  }
+
+  interface HTMLElement {
+    msRequestFullScreen?: () => Promise<void>;
+    mozRequestFullScreen?: () => Promise<void>;
+    webkitRequestFullScreen?: () => Promise<void>;
+  }
+}
+
 export function initFullscreenControl(
   map: google.maps.Map,
   fullScreenControl: HTMLElement,
@@ -7,9 +27,9 @@ export function initFullscreenControl(
 
   fullScreenControl.onclick = function () {
     if (isFullscreen(fullScreenElement)) {
-      document.exitFullscreen();
+      exitFullscreen();
     } else {
-      fullScreenElement.requestFullscreen();
+      requestFullscreen(fullScreenElement);
     }
   };
   document.onfullscreenchange = function () {
@@ -22,5 +42,34 @@ export function initFullscreenControl(
 }
 
 function isFullscreen(element: HTMLElement) {
-  return document.fullscreenElement === element;
+  return (
+    (document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement) === element
+  );
+}
+
+function requestFullscreen(element: HTMLElement) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullScreen) {
+    element.msRequestFullScreen();
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
 }
